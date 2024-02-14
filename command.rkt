d61b17131c4e7cafbc15e427e734de177c33ee67
Silvio Mayolo <mercerenies@comcast.net>
2024-02-14 15:33:28 -0500

    Initial commit

---
+++ command.rkt
@@ -0,0 +1,66 @@
+
+#lang racket
+
+(struct command (ring-name index) #:transparent)
+
+(define ring/c
+  (or/c 'operations 'math))
+
+(define noop 0)
+
+(define op/noop (command 'operations noop))
+(define op/exit (command 'operations 1))
+(define op/set-to-one (command 'operations 2))
+(define op/set-to-zero (command 'operations 3))
+(define op/load-memory (command 'operations 4))
+(define op/store-memory (command 'operations 5))
+(define op/add-program (command 'operations 6))
+(define op/add-memory (command 'operations 7))
+(define op/logical-and (command 'operations 8))
+(define op/add-program-if (command 'operations 9))
+(define op/integer-io (command 'operations 10))
+(define op/ascii-io (command 'operations 11))
+
+(define math/noop (command 'math noop))
+(define math/load-memory (command 'math 1))
+(define math/store-memory (command 'math 2))
+(define math/+ (command 'math 3))
+(define math/* (command 'math 4))
+(define math/div (command 'math 5))
+(define math/set-to-zero (command 'math 6))
+(define math/< (command 'math 7))
+(define math/= (command 'math 8))
+(define math/> (command 'math 9))
+(define math/not (command 'math 10))
+(define math/negate (command 'math 11))
+
+(provide
+ (contract-out
+  [struct command
+    ((ring-name ring/c) (index integer?))]
+  [ring/c contract?]
+  [noop integer?]
+  [op/noop command?]
+  [op/exit command?]
+  [op/set-to-one command?]
+  [op/set-to-zero command?]
+  [op/load-memory command?]
+  [op/store-memory command?]
+  [op/add-program command?]
+  [op/add-memory command?]
+  [op/logical-and command?]
+  [op/add-program-if command?]
+  [op/integer-io command?]
+  [op/ascii-io command?]
+  [math/noop command?]
+  [math/load-memory command?]
+  [math/store-memory command?]
+  [math/+ command?]
+  [math/* command?]
+  [math/div command?]
+  [math/set-to-zero command?]
+  [math/< command?]
+  [math/= command?]
+  [math/> command?]
+  [math/not command?]
+  [math/negate command?]))
---
+++ whirlgen.rkt
@@ -0,0 +1,38 @@
+
+#lang racket
+
+;; Generator script for programs in the Whirl programming language.
+
+;; Conventions to make this actually remotely sane.
+;;
+;; (1) Assume all wheels are facing clockwise at all times. Anytime I
+;; need to execute something, I'll double-rotate so that we're
+;; clockwise after execution.
+;;
+;; (2) Whenever we do a jump, the math wheel should be on NOOP, and
+;; the op wheel should be on IF. Anytime we end up at a jump target
+;; (even if we just came from before that position), this should be
+;; the case. That means that trivial jumps of length zero are fair
+;; game.
+;;
+;; (3) As a corollary to the above, the P-ADD instruction is outright
+;; banned. There is no safe way to do an unconditional jump and end up
+;; in a consistent state. If I need to unconditionally jump, I'll do a
+;; conditional jump with a constant condition.
+;;
+;; (4) Data is stored at the even positions (0-indexed) in the memory
+;; buffer. ALL odd positions in the memory buffer are assumed to be
+;; garbage temporaries. This is so that I can generate constant
+;; numbers easily and move around.
+;;
+;; (5) Variables are stored at the low magnitude even positions. Our
+;; big array is stored at the trailing right end of the memory buffer.
+;; Again, all useful values are stored at even positions, so to move
+;; to an array position we'll need to multiply the desired index by 2.
+;;
+;; (6) The largest variable that is NOT part of the trailing array is
+;; the constant -1, which we'll use to move left in memory.
+
+(require "command.rkt")
+
+(println op/exit)
