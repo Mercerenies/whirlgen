901aa390cdab92323721c39c66e67d47a3cdb0f0
Silvio Mayolo <mercerenies@comcast.net>
2024-02-14 20:38:14 -0500

    Problem-specific variables

---
+++ euler179.rkt
@@ -0,0 +1,13 @@
+
+#lang racket
+
+;; Specific code for Project Euler 179
+
+(require "structures.rkt")
+
+(define var/i (var 'i 0))
+(define var/j (var 'j 2))
+(define var/p (var 'p 4))
+(define var/valuation (var 'valuation 6))
+(define var/-1 (var 'minus-one 8)) ; Must be the largest non-array variable
+
