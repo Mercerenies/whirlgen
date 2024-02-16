795f7b9fc5ac81ca5f855539725f59cb465b4bdb
Silvio Mayolo <mercerenies@comcast.net>
2024-02-15 21:09:42 -0500

    Remove unnecessary conditional check

--- euler179.rkt
+++ euler179.rkt
@@ -36,7 +36,7 @@
 (define length-of-jump-instructions 900)
 
 (define (setup-limit-constant)
-  (store-constant var/limit 200001)) ; 10000001
+  (store-constant var/limit 400001)) ;; 10000001
 
 (define (print-newline)
   (code (store-constant var/constant 10)
@@ -79,10 +79,7 @@
         ;; Update all multiples of the prime i.
         (assign var/j var/i)
         (do-while-nonzero var/condition var/tmp1
-          (array-get array-start-index var/array-value var/j var/tmp1 var/tmp2)
-          (cmp< var/array-value var/i)
-          (if-nonzero var/array-value var/tmp1
-            (array-set array-start-index var/i var/j var/tmp1 var/tmp2))
+          (array-set array-start-index var/i var/j var/tmp1 var/tmp2)
           (add var/j var/i)
           (assign var/condition var/j)
           (cmp< var/condition var/limit)))
